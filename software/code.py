038f16015411c36ade54f2fbc386b388bad93139
Bruno Henrique Ferreira Pinto <bhfp@windows.cin.ufpe.br>
2024-07-31 18:52:42 -0300

    Update code.py

---
+++ software/code.py
@@ -0,0 +1,7 @@
+velocidade = 40                # Velocidade atual do veículo
+velocidade_permitida = 60      # Velocidade máxima permitida
+
+if (velocidade_permitida <= velocidade): 
+    alta_velocidade = 1
+else:
+    alta_velocidade = 0
