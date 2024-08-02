6da51b28aa04b88a9cef6e878977e4c492817b80
Bruno Henrique <brunohenriquef.pinto@gmail.com>
2024-08-02 11:53:49 -0300

    update code.asm

--- software/mips.asm
+++ software/mips.asm
@@ -10,18 +10,4 @@ LW $s1, 4($t0)  # velocidade_permitida = 60
 
 SLT $s2, $s1, $s0   # $s2 = (velocidade_permitida < velocidade)
 
-BEQ $s2, $t0, set_one  # brach to set_one if velocidade < velocidade_permitida
-
-# if velocidade > velocidade_permitida
-
-LI $t1, 1
-SW $t1, 12($t0)
-
-J end
-
-set_one:
-    LI $t1, 0
-    SW $t1, 12($t0)
-
-end:
-    LW $s3, 12($t0)
+SW $s2, 12($t0)
